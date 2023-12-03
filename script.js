394b241c20ee8853d2e14adb795f576ab3939ea9
Naufall1302 <152760441+Naufall1302@users.noreply.github.com>
2023-12-03 11:42:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const wrapper document.querySelector(".wrapper");
+
+const question document.querySelector(".question");
+
+const gif document.querySelector(".gif"); const yesBtn = document.querySelector(".yes-btn");
+
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () {
+
+question.innerHTML = "Aaaaa, I like you too";
+
+gif.src =
+
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+
+noBtn.addEventListener("mouseover", () => {
+
+const noBtnRect noBtn.getBoundingClientRect();
+
+const maxX = window.innerWidth noßtnRect.width;
+
+const maxy window.innerHeight noßtnRect.height;
+
+const randomX = Math.floor(Math.random() maxx);
+
+const randomY = Math.floor(Math.random() maxY);
+
+noBtn.style.left = randomX + "px";
+
+noBtn.style.top = randomY + "px";
+
+});
